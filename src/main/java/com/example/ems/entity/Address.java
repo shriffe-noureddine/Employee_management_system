52c1fd036a538a0160a6fa8cde44ce8b55d50a7f
shriffe-noureddine <noureddineshriffe@gmail.com>
2025-09-01 01:56:18 +0200

    create address entity

---
+++ src/main/java/com/example/ems/entity/Address.java
@@ -0,0 +1,22 @@
+package com.example.ems.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+}
